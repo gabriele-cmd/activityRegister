afc4b0a4168b8b4974be9df6eb4c2da95c48b6b1
gabriele-cmd <chirligabriele03@gmail.com>
2023-07-12 17:14:25 +0200

    modified operations on Register adding new functions and their testings

--- Register.cpp
+++ Register.cpp
@@ -20,3 +20,48 @@ bool Register::isNotActivity(const QDate &date){
 QMultiMap<QDate, Activity> Register::getActivities() const{
     return activities;
 }
+
+
+void Register::listActivities(QDate& d) const{
+    auto range = activities.equal_range(d);
+    for (auto it = range.first; it != range.second; ++it) {
+        qDebug() << "Date: " << it.key() << ", Start time: " << it.value().getStartTime() << ", End time: " << it.value().getEndTime() << ", Activity: " << it.value().getDescription();
+    }
+}
+
+bool Register::findActivity(Activity& a) const{
+    for(auto it = activities.begin(); it != activities.end(); it++){
+        if(it.value() == a)
+            return true;
+    }
+    return false;
+}
+
+void Register::modifyActivity(Activity& a, QDate d, QTime st, QTime et, QString desc){
+    for(auto it = activities.begin(); it != activities.end(); it++){
+        if(it.value() == a){
+            it.value().setDate(d);
+            it.value().setStartTime(st);
+            it.value().setEndTime(et);
+            it.value().setDescription(desc);
+        }
+    }
+}
+
+QMultiMap<QDate, Activity> Register::getUncompletedActivities() const {
+    QMultiMap<QDate, Activity> uncompleted;
+    for (auto it = activities.begin(); it != activities.end(); it++) {
+        if ((it.value().getDate() > QDate::currentDate()) || (it.value().getDate() == QDate::currentDate() && it.value().getEndTime() > QTime::currentTime()))
+            uncompleted.insert(it.key(), it.value());
+    }
+    return uncompleted;
+}
+
+QMultiMap<QDate, Activity> Register::getCompletedActivities() const {
+    QMultiMap<QDate, Activity> completed;
+    for (auto it = activities.begin(); it != activities.end(); it++) {
+        if ((it.value().getDate() < QDate::currentDate()) || (it.value().getDate() == QDate::currentDate() && it.value().getEndTime() < QTime::currentTime()))
+            completed.insert(it.key(), it.value());
+    }
+    return completed;
+}
--- test/TestRegister.cpp
+++ test/TestRegister.cpp
@@ -33,13 +33,94 @@ TEST(TestRegister, testRegisterAddActivity) {
 
 TEST(TestRegister, testRegisterRemoveActivity) {
     Register *r = new Register();
+
+
     Activity *a1 = new Activity("Descrizione1", QTime(12,00), QTime(13,00), QDate(2023,7,9));
     Activity *a2 = new Activity("Descrizione2", QTime(15,00), QTime(16,00), QDate(2023,8,12));
+    Activity *a3 = new Activity("Descrizione3", QTime(15,00), QTime(16,00), QDate(2023,8,12));
     r->addActivity(a1->getDate(), *a1);
     r->addActivity(a2->getDate(), *a2);
     r->removeActivity(*a1);
+    r->removeActivity(*a3);
+
 
     ASSERT_FALSE(r->isNotActivity(a2->getDate()));
     ASSERT_TRUE(r->isNotActivity(a1->getDate()));
     ASSERT_EQ(1, r->getActivities().size());
+}
+
+//tests finding an activity
+TEST(TestRegister, testRegisterFindActivity) {
+    Register *r = new Register();
+    Activity *a1 = new Activity("Descrizione1", QTime(12,00), QTime(13,00), QDate(2023,7,9));
+    Activity *a2 = new Activity("Descrizione2", QTime(15,00), QTime(16,00), QDate(2023,8,12));
+    r->addActivity(a1->getDate(), *a1);
+    r->addActivity(a2->getDate(), *a2);
+
+    ASSERT_TRUE(r->findActivity(*a1));
+    ASSERT_TRUE(r->findActivity(*a2));
+}
+
+//tests getting uncompleted activities
+TEST(TestRegister, testGetUncompletedActivities){
+    Register *r = new Register();
+    Activity *a1 = new Activity("Descrizione1", QTime(12,00), QTime(13,00), QDate(2023,7,9));
+    Activity *a2 = new Activity("Descrizione2", QTime(15,00), QTime(16,00), QDate(2023,8,16));
+    Activity *a3 = new Activity("Descrizione3", QTime(15,00), QTime(16,00), QDate(2023,8,23));
+    r->addActivity(a1->getDate(), *a1);
+    r->addActivity(a2->getDate(), *a2);
+    r->addActivity(a3->getDate(), *a3);
+
+    ASSERT_EQ(2, r->getUncompletedActivities().size());
+}
+
+//tests getting completed activities
+TEST(TestRegister, testGetCompletedActivities){
+    Register *r = new Register();
+    Activity *a1 = new Activity("Descrizione1", QTime(12,00), QTime(13,00), QDate(2023,7,9));
+    Activity *a2 = new Activity("Descrizione2", QTime(15,00), QTime(16,00), QDate(2023,8,16));
+    Activity *a3 = new Activity("Descrizione3", QTime(15,00), QTime(16,00), QDate(2023,8,23));
+    r->addActivity(a1->getDate(), *a1);
+    r->addActivity(a2->getDate(), *a2);
+    r->addActivity(a3->getDate(), *a3);
+
+    ASSERT_EQ(1, r->getCompletedActivities().size());
+}
+
+//tests modifying an activity
+TEST(TestRegister, testModifyActivity){
+    Register *r = new Register();
+    Activity *a1 = new Activity("Descrizione1", QTime(12,00), QTime(13,00), QDate(2023,7,9));
+    Activity *a2 = new Activity("Descrizione2", QTime(15,00), QTime(16,00), QDate(2023,8,16));
+    r->addActivity(a1->getDate(), *a1);
+    r->addActivity(a2->getDate(), *a2);
+
+    ASSERT_EQ(2, r->getActivities().size());
+    ASSERT_EQ("Descrizione1", r->getActivities().value(a1->getDate(), *a1).getDescription());
+    ASSERT_EQ("Descrizione2", r->getActivities().value(a2->getDate(), *a2).getDescription());
+
+    r->modifyActivity(*a1,  QDate(2023,7,9), QTime(12,00), QTime(13,00), "Descrizione1 modificata");
+
+    ASSERT_EQ(2, r->getActivities().size());
+    ASSERT_EQ("Descrizione1 modificata", r->getActivities().value(a1->getDate(), *a1).getDescription());
+    ASSERT_EQ("Descrizione2", r->getActivities().value(a2->getDate(), *a2).getDescription());
+}
+
+//tests removing an activity
+TEST(TestRegister, testRemovingUnexistingActivity){
+    Register *r = new Register();
+    Activity *a1 = new Activity("Descrizione1", QTime(12,00), QTime(13,00), QDate(2023,7,9));
+    Activity *a2 = new Activity("Descrizione2", QTime(15,00), QTime(16,00), QDate(2023,8,16));
+    r->addActivity(a1->getDate(), *a1);
+    r->addActivity(a2->getDate(), *a2);
+
+    ASSERT_EQ(2, r->getActivities().size());
+    ASSERT_EQ("Descrizione1", r->getActivities().value(a1->getDate(), *a1).getDescription());
+    ASSERT_EQ("Descrizione2", r->getActivities().value(a2->getDate(), *a2).getDescription());
+
+    r->removeActivity(*a1);
+    r->removeActivity(*a1);
+
+    ASSERT_EQ(1, r->getActivities().size());
+    ASSERT_EQ("Descrizione2", r->getActivities().value(a2->getDate(), *a2).getDescription());
 }
